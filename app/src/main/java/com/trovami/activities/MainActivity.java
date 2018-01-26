c08163c9e4f6932463660f490813f6486d6bd821
Samrat <samratshekhar42@gmail.com>
2018-01-27 02:34:10 +0530

    Update app to use dataBinding

--- app/src/main/java/com/trovami/activities/MainActivity.java
+++ app/src/main/java/com/trovami/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.trovami.activities;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,18 +11,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.trovami.R;
+import com.trovami.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(binding.toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
