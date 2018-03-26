f1a704b7a3f34762f2933ecec9f93a0d48fb4b32
Kuntala Sett <kuntala.sett.666@gmail.com>
2018-03-26 23:24:28 +0530

    add util method to check if service is running

--- app/src/main/java/com/trovami/utils/Utils.java
+++ app/src/main/java/com/trovami/utils/Utils.java
@@ -1,8 +1,23 @@
 package com.trovami.utils;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+
 /**
  * Created by samrat on 27/01/18.
  */
 
 public class Utils {
+
+    public static boolean isServiceRunning(Activity activity, Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
