32f2e501973c7242dd224806d244828dd3578c05
anushalihala <35848893+anushalihala@users.noreply.github.com>
2018-04-06 23:14:09 +0530

    update user schema

--- app/src/main/java/com/trovami/models/RDBSchema.java
+++ app/src/main/java/com/trovami/models/RDBSchema.java
@@ -8,10 +8,14 @@ public class RDBSchema {
     public class Users {
         public static final String TABLE_NAME = "users";
         public static final String UID = "uid";
-        public static final  String NAME = "name";
-        public static final  String EMAIL = "email";
-        public static final  String PHOTO_URL = "photoUrl";
-        public static final  String LATLONG = "latLong";
+        public static final String NAME = "name";
+        public static final String EMAIL = "email";
+        public static final String PHONE = "phone";
+        public static final String GENDER = "gender";
+        public static final String PHOTO_URL = "photoUrl";
+        public static final String LATLONG = "latLong";
+        public static final String FOLLOWER = "follower";
+        public static final String FOLLOWING = "following";
     }
 
     public class LatLong {
