7a116355062b11d0c7eaf1a862f35c07b997cb11
Samrat <samratshekhar42@gmail.com>
2018-04-18 00:27:45 +0530

    integrate user fetch per child

--- app/src/main/java/com/trovami/adapters/HomeItemViewHolder.java
+++ app/src/main/java/com/trovami/adapters/HomeItemViewHolder.java
@@ -5,7 +5,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bignerdranch.expandablerecyclerview.ChildViewHolder;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.trovami.R;
+import com.trovami.models.User;
+
+import java.util.Iterator;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,10 +30,31 @@ public class HomeItemViewHolder extends ChildViewHolder {
         txtItemTitle = itemView.findViewById(R.id.tV_ListTitle);
         txtItemSubtitle = itemView.findViewById(R.id.tV_ListSubtitle);
         cimgPhoto=itemView.findViewById(R.id.cImg_ListPhoto);
-        layoutListParent=itemView.findViewById(R.id.linear_ItemParent);;
+        layoutListParent=itemView.findViewById(R.id.linear_ItemParent);
     }
 
     public void bind(String uid) {
-        txtItemTitle.setText(uid);
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterator<DataSnapshot> iterator = dataSnapshot.getChildren().iterator();
+                if(iterator.hasNext()) {
+                    // user found, fetch followers and following
+                    DataSnapshot singleSnapshot = iterator.next();
+                    User user = singleSnapshot.getValue(User.class);
+//                    fragment.fetchFollowLists(user.following, user.follower);
+                    txtItemTitle.setText(user.name);
+                } else {
+                    txtItemTitle.setText(null);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        User.getUserById(uid, listener);
+//        txtItemTitle.setText(uid);
     }
 }
