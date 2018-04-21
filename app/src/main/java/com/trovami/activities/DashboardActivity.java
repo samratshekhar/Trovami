80f8cca52463115f8da61fd902bf1cb6887332f1
Samrat <samratshekhar42@gmail.com>
2018-04-22 02:03:49 +0530

    integrate data to nav header

--- app/src/main/java/com/trovami/activities/DashboardActivity.java
+++ app/src/main/java/com/trovami/activities/DashboardActivity.java
@@ -2,12 +2,10 @@ package com.trovami.activities;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.FragmentTransaction;
-import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -15,10 +13,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,23 +29,21 @@ import com.google.firebase.database.ValueEventListener;
 import com.trovami.R;
 import com.trovami.databinding.ActivityDashboardBinding;
 import com.trovami.fragments.HomeFragment;
-import com.trovami.fragments.MapFragment;
 import com.trovami.fragments.NotificationFragment;
 import com.trovami.fragments.UserFragment;
-import com.trovami.models.Notification;
 import com.trovami.models.User;
 import com.trovami.services.LocationFetchService;
 import com.trovami.utils.Utils;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "DashboardActivity";
     private ActivityDashboardBinding mBinding;
+    private FirebaseAuth mAuth;
+    private User mCurrentUser;
 
     private HomeFragment mHomeFragment;
     private NotificationFragment mNotificationFragment;
@@ -62,7 +61,8 @@ public class DashboardActivity extends AppCompatActivity
         } else {
             Log.d(TAG, "Service up");
         }
-
+        setupFirebaseAuth();
+        fetchCurrentUser();
     }
 
     @Override
@@ -70,32 +70,73 @@ public class DashboardActivity extends AppCompatActivity
         super.onResume();
     }
 
+    private void setupFirebaseAuth() {
+        mAuth = FirebaseAuth.getInstance();
+    }
 
     private void setupUI() {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard);
         setSupportActionBar(mBinding.toolbar);
 
-        DrawerLayout drawer = mBinding.drawerLayout;
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, mBinding.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this,
+                mBinding.drawerLayout,
+                mBinding.toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        mBinding.drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = mBinding.navView;
-        navigationView.setNavigationItemSelectedListener(this);
+        mBinding.navView.setNavigationItemSelectedListener(this);
+    }
 
+    private void fetchCurrentUser() {
+        if (mCurrentUser != null) {
+            updateUI();
+        } else {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            ValueEventListener listener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Iterator<DataSnapshot> iterator = dataSnapshot.getChildren().iterator();
+                    if(iterator.hasNext()) {
+                        // user found, fetch followers and following
+                        DataSnapshot singleSnapshot = iterator.next();
+                        mCurrentUser = singleSnapshot.getValue(User.class);
+                        updateUI();
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // TODO: handle error
+                }
+            };
+            User.getUserById(currentUser.getUid(), listener);
+        }
+    }
+
+    private void updateUI() {
         final Activity activity = this;
-        mBinding.fab.setOnClickListener(new View.OnClickListener() {
+        // TODO: update porfile pic
+        View headerView =  mBinding.navView.getHeaderView(0);
+        ImageView profileImageView = headerView.findViewById(R.id.nav_image_view);
+        TextView nameTextView = headerView.findViewById(R.id.nav_title_text_view);
+        TextView emailTextView = headerView.findViewById(R.id.nav_subtitle_text_view);
+
+        nameTextView.setText(mCurrentUser.name);
+        emailTextView.setText(mCurrentUser.email);
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                User user = new User();
-                user.name = "Samrat";
-                user.email = null;
                 Intent intent = new Intent(activity, ProfileActivity.class);
-                intent.putExtra("user", user);
+                intent.putExtra("user", mCurrentUser);
                 intent.putExtra("isUpdate", true);
                 startActivity(intent);
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
             }
         });
+
     }
 
     @Override
@@ -106,6 +147,24 @@ public class DashboardActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
+    }
+
+    private void logout() {
+        final Activity activity = this;
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Sign out from Trovami?");
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                Intent logoutIntent = new Intent(activity, MainActivity.class);
+                startActivity(logoutIntent);
+                finish();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("No", null);
+        alertDialogBuilder.create().show();
     }
 
     @Override
@@ -132,11 +191,7 @@ public class DashboardActivity extends AppCompatActivity
         } else if (id == R.id.nav_about) {
 
         } else if (id == R.id.nav_logout) {
-            FirebaseAuth mAuth = FirebaseAuth.getInstance();
-            mAuth.signOut();
-            Intent logoutIntent = new Intent(this, MainActivity.class);
-            startActivity(logoutIntent);
-            finish();
+            logout();
         }
 
         try {
@@ -146,8 +201,7 @@ public class DashboardActivity extends AppCompatActivity
             e.printStackTrace();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        mBinding.drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 }
